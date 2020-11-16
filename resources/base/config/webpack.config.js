38ff511728374d7b77b5fd6f2fad96a4f6396cb7
maizhichao <zhichao.mai@se.com>
2020-11-16 15:14:05 +0800

    Enable importmap-overrides widget by default.

--- resources/base/config/webpack.config.js
+++ resources/base/config/webpack.config.js
@@ -297,20 +297,7 @@ module.exports = function (webpackEnv) {
             preset: ["default", { minifyFontValues: { removeQuotes: false } }]
           }
         })
-      ],
-      // Automatically split vendor and commons
-      // https://twitter.com/wSokra/status/969633336732905474
-      // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-      splitChunks: {
-        chunks: "all",
-        name: false
-      }
-      // // Keep the runtime chunk separated to enable long term caching
-      // // https://twitter.com/wSokra/status/969679223278505985
-      // // https://github.com/facebook/create-react-app/issues/5358
-      // runtimeChunk: {
-      //   name: entrypoint => `runtime-${entrypoint.name}`
-      // }
+      ]
     },
     resolve: {
       // This allows you to set a fallback for where webpack should look for modules.
